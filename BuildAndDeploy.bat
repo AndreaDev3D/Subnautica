30c0a2b77aa57dd72e0df516c4484bc0451bbbc3
AndreaDev3D <andreapomposelli@gmail.com>
2024-09-04 09:24:30 +0100

    chore: Add targets to project based on configurations

--- BuildAndDeploy.bat
+++ BuildAndDeploy.bat
@@ -1,39 +1,54 @@
-@echo off
+@echo on
 REM Set variables based on arguments passed to the script
 set projectName=%1
 set buildPath=%2
 set projectDir=%3
 set targetDir=%4
-set subnauticaFolder=D:\Steam\steamapps\common\SubnauticaZero\BepInEx\plugins\%projectName%\
-set zipDestination=%3..\Download\%projectName%.zip
+set Configuration=%~5
+set subnauticaFolder=D:\Steam\steamapps\common\Subnautica\BepInEx\plugins\%projectName%\
+set subnauticaZeroFolder=D:\Steam\steamapps\common\SubnauticaZero\BepInEx\plugins\%projectName%\
+set zipDestination=%3..\Download\%Configuration%\%projectName%.zip
 
 REM Debugging: Echo paths to verify
 echo projectName=%projectName%
 echo buildPath=%buildPath%
 echo projectDir=%projectDir%
 echo targetDir=%targetDir%
-echo subnauticaFolder=%subnauticaFolder%
+echo Configuration=%Configuration%
+
+REM Determine the correct folder based on Configuration
+if "%Configuration%"=="SN" (
+    set targetFolder=%subnauticaFolder%
+) else if "%Configuration%"=="BZ" (
+    set targetFolder=%subnauticaZeroFolder%
+) else (
+    echo "Unknown Configuration: %Configuration%"
+    pause
+    exit /b 1
+)
+
+echo targetFolder=%targetFolder%
 
 REM Create directories if they do not exist
-if not exist "%subnauticaFolder%\Assets" (
-    mkdir "%subnauticaFolder%\Assets"
+if not exist "%targetFolder%\Assets" (
+    mkdir "%targetFolder%\Assets"
 )
 
 REM Copy build files
-xcopy "%buildPath%" "%subnauticaFolder%" /y
-xcopy "%projectDir%*.json" "%subnauticaFolder%" /y
-xcopy "%projectDir%Assets\*.asset" "%subnauticaFolder%\Assets\" /y
-xcopy "%projectDir%Assets\*.manifest" "%subnauticaFolder%\Assets\" /y
-xcopy "%projectDir%Assets\*.png" "%subnauticaFolder%\Assets\" /y
+xcopy "%buildPath%" "%targetFolder%" /y
+xcopy "%projectDir%*.json" "%targetFolder%" /y
+xcopy "%projectDir%Assets\*.asset" "%targetFolder%\Assets\" /y
+xcopy "%projectDir%Assets\*.manifest" "%targetFolder%\Assets\" /y
+xcopy "%projectDir%Assets\*.png" "%targetFolder%\Assets\" /y
 
 REM Copy AD3D_Common.dll file
-xcopy "%targetDir%\AD3D_Common.dll" "%subnauticaFolder%" /y
-xcopy "%targetDir%\Assets\*.asset" "%subnauticaFolder%\Assets\" /y
-xcopy "%targetDir%\Assets\*.manifest" "%subnauticaFolder%\Assets\" /y
-xcopy "%targetDir%\Assets\*.png" "%subnauticaFolder%\Assets\" /y
+xcopy "%targetDir%\AD3D_Common.dll" "%targetFolder%" /y
+xcopy "%targetDir%\Assets\*.asset" "%targetFolder%\Assets\" /y
+xcopy "%targetDir%\Assets\*.manifest" "%targetFolder%\Assets\" /y
+xcopy "%targetDir%\Assets\*.png" "%targetFolder%\Assets\" /y
 
 REM Zip the mod folder
-7z a -tzip "%zipDestination%" "%subnauticaFolder%" -mx9
+7z a -tzip "%zipDestination%" "%targetFolder%" -mx9
 
 REM Notify completion
 echo Mod build and deployment complete.
