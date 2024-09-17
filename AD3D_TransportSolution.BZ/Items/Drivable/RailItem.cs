ee44ab353c94a9a5e328ebddef7a1a6aedd96f39
AndreaDev3D <andreapomposelli@gmail.com>
2024-09-17 15:06:37 +0100

    chore: Add Transport Solutions project

---
+++ AD3D_TransportSolution.BZ/Items/Drivable/RailItem.cs
@@ -0,0 +1,71 @@
+﻿using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Crafting;
+using Nautilus.Extensions;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace AD3D_TransportSolution.BZ.Items.Drivable
+{
+    public class RailItem
+    {
+        public PrefabInfo PrefabInfo { get; }
+
+        public RailItem(string classID, string friendlyName, string shortDescription)
+        {
+            PrefabInfo = PrefabInfo
+            .WithTechType(classID, friendlyName, shortDescription, unlockAtStart: true)
+            .WithIcon(ImageUtils.LoadSpriteFromTexture(Plugin.AssetBundle.LoadAsset<Texture2D>($"{classID}.png")));
+        }
+
+        public void Register()
+        {
+            var customPrefab = new CustomPrefab(PrefabInfo);
+
+            customPrefab.SetGameObject(GetAssetBundlePrefab());
+
+            var recipe = new RecipeData()
+            {
+                craftAmount = 1,
+                Ingredients =
+                {
+                    new Ingredient(TechType.Titanium, 1),
+                },
+            };
+
+            customPrefab.SetRecipe(recipe)
+                .WithFabricatorType(CraftTree.Type.Constructor)
+                .WithStepsToFabricatorTab("Exterior Modules");
+
+            customPrefab.SetEquipment(EquipmentType.Hand);
+            customPrefab.SetPdaGroupCategory(TechGroup.ExteriorModules, TechCategory.ExteriorModule);
+
+            customPrefab.Register();
+        }
+
+        private GameObject GetAssetBundlePrefab()
+        {
+            var prefab = Plugin.AssetBundle.LoadAsset<GameObject>($"{PrefabInfo.ClassID}.prefab");
+            PrefabUtils.AddBasicComponents(prefab, PrefabInfo.ClassID, PrefabInfo.TechType, LargeWorldEntity.CellLevel.Far);
+            MaterialUtils.ApplySNShaders(prefab);
+
+            SetupConstructable(prefab);
+
+            return prefab;
+        }
+
+        private void SetupConstructable(GameObject prefab)
+        {
+            var rootModel = prefab.SearchChild("model");
+            var constructable = PrefabUtils.AddConstructable(prefab, PrefabInfo.TechType, ConstructableFlags.Outside, rootModel);
+            constructable.allowedOnConstructables = true;
+            constructable.allowedOutside = true;
+            constructable.allowedOnGround = true;
+            constructable.forceUpright = true;
+            constructable.rotationEnabled = true;
+            constructable.placeDefaultDistance = 7.5f;
+            constructable.placeMinDistance = 5f;
+            constructable.placeMaxDistance = 15f;
+        }
+    }
+}
---
+++ AD3D_TransportSolution.BZ/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using AD3D_TransportSolution.BZ.Items.Drivable;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Utility;
+using System.Reflection;
+using UnityEngine;
+
+namespace AD3D_TransportSolution.BZ
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        public static AssetBundle AssetBundle { get; private set; }
+
+        private void Awake()
+        {
+            AssetBundle = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "transportsolutions.asset");
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // Initialize custom prefabs
+            InitializePrefabs();
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void InitializePrefabs()
+        {
+            //YeetKnifePrefab.Register();
+            var Monorail_Straight = new RailItem("Monorail_Straight", "Monorail Straight", "Streight Rail");
+            Monorail_Straight.Register();
+
+            var Monorail_Curve = new RailItem("Monorail_Curve", "Monorail_Curve", "Monorail Curve");
+            Monorail_Curve.Register();
+
+            var Monorail_Pod = new RailItem("Monorail_Pod", "Monorail Pod", "Monorail Pod");
+            Monorail_Pod.Register();
+        }
+    }
+}
